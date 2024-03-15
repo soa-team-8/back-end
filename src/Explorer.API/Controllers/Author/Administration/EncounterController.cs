e055f27b3859a3a5f122b5ca07811bfc21aa6423
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2024-03-15 09:41:07 +0100

    feat: encounter controler create method update

--- src/Explorer.API/Controllers/Author/Administration/EncounterController.cs
+++ src/Explorer.API/Controllers/Author/Administration/EncounterController.cs
@@ -2,10 +2,14 @@
 using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Encounters.API.Dtos;
 using Explorer.Encounters.API.Public;
+using Explorer.Stakeholders.API.Public;
 using Explorer.Stakeholders.Infrastructure.Authentication;
 using Explorer.Tours.API.Dtos;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
 
 namespace Explorer.API.Controllers.Author.Administration
 {
@@ -17,7 +21,7 @@ namespace Explorer.API.Controllers.Author.Administration
     {
         private readonly IEncounterService _encounterService;
         private readonly ImageService _imageService;
-
+        private readonly HttpClient Client = new HttpClient();
 
         public EncounterController(IEncounterService encounterService)
         {
@@ -28,18 +32,34 @@ namespace Explorer.API.Controllers.Author.Administration
 
 
         [HttpPost]
-        public ActionResult<EncounterDto> Create([FromForm] EncounterDto encounter,[FromQuery] long checkpointId, [FromQuery] bool isSecretPrerequisite, [FromForm] List<IFormFile>? imageF = null)
+        public async Task<ActionResult<EncounterDto>> Create([FromForm] EncounterDto encounter,[FromQuery] long checkpointId, [FromQuery] bool isSecretPrerequisite, [FromForm] List<IFormFile>? imageF = null)
         {
 
+            using var formData = new MultipartFormDataContent();
+            var jsonContent = new StringContent(JsonSerializer.Serialize(encounter));
+            formData.Add(jsonContent, "encounter");
+
+            // Dodajte slike u multipart form data
             if (imageF != null && imageF.Any())
             {
-                var imageNames = _imageService.UploadImages(imageF);
-                if (encounter.Type =="Location")
-                    encounter.Image = imageNames[0];
+                foreach (var imageFile in imageF)
+                {
+                    var imageStream = imageFile.OpenReadStream();
+                    var imageContent = new StreamContent(imageStream);
+                    formData.Add(imageContent, "pictures", imageFile.FileName);
+                }
             }
 
-            var result = _encounterService.Create(encounter, checkpointId, isSecretPrerequisite,User.PersonId());
-            return CreateResponse(result);
+            var response = await Client.PostAsync($"http://localhost:3000/encounters/author", formData);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var encId = (int)JObject.Parse(jsonResponse)["id"];
+                response = await Client.PutAsync($"http://localhost:3000/checkpoints/setEnc/{checkpointId}/{encId}/{isSecretPrerequisite}/{encounter.AuthorId}", formData);
+            }
+
+            jsonResponse = await response.Content.ReadAsStringAsync();
+            return CreateResponse(jsonResponse.ToResult());
         }
 
         [HttpPut]
