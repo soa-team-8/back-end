4078c492ad3d6d2d41fcdc8326c4244fd77ea8a5
VukPavlovic425 <vuk.pavlovic425@gmail.com>
2024-03-18 13:50:36 +0100

    feat: published tours api

--- src/Explorer.API/Controllers/Tourist/Tour/PublishedToursController.cs
+++ src/Explorer.API/Controllers/Tourist/Tour/PublishedToursController.cs
@@ -3,6 +3,7 @@ using Explorer.Tours.API.Public.Administration;
 using Explorer.Tours.API.Public.Recommendation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Explorer.API.Controllers.Tourist.Tour
 {
@@ -10,34 +11,46 @@ namespace Explorer.API.Controllers.Tourist.Tour
     [Route("api/tourist/shopping")]
     public class PublishedToursController : BaseApiController
     {
-        private readonly ITourService _tourService;
+        private readonly HttpClient _httpClient;
+
         private readonly ITourRecommendationService _tourRecommendationService;
 
-        public PublishedToursController(ITourService tourService, ITourRecommendationService tourRecommendationService)
+        public PublishedToursController(IHttpClientFactory httpClientFactory, ITourRecommendationService tourRecommendationService)
         {
-            _tourService = tourService;
             _tourRecommendationService = tourRecommendationService;
+
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:3000");
         }
 
         [HttpGet]
-        public ActionResult<List<TourPreviewDto>> GetPublishedTours([FromQuery] int page, [FromQuery] int pageSize)
+        public async Task<ActionResult<List<TourPreviewDto>>> GetPublishedTours([FromQuery] int page, [FromQuery] int pageSize)
         {
-            var result = _tourService.GetFilteredPublishedTours(page, pageSize);
-            return CreateResponse(result);
+            var response = await _httpClient.GetAsync("/published-tours");
+            response.EnsureSuccessStatusCode();
+            var contentString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<TourPreviewDto>>(contentString);
+            return Ok(result);
         }
 
         [HttpGet("details/{id:int}")]
-        public ActionResult<List<TourPreviewDto>> GetPublishedTour(int id)
+        public async Task<ActionResult<List<TourPreviewDto>>> GetPublishedTour(int id)
         {
-            var result = _tourService.GetPublishedTour(id);
-            return CreateResponse(result);
+            var response = await _httpClient.GetAsync($"/published-tours/{id}");
+            response.EnsureSuccessStatusCode();
+            var contentString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<TourPreviewDto>>($"[{contentString}]");
+            return Ok(result);
         }
 
         [HttpGet("averageRating/{tourId:int}")]
-        public double GetAverageRating(long tourId)
+        public async Task<ActionResult<double>> GetAverageRating(long tourId)
         {
-            var result = _tourService.GetAverageRating(tourId);
-            return result;
+            var response = await _httpClient.GetAsync($"/published-tours/{tourId}/rating");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<double>(content);
+            return Ok(result);
         }
 
         [HttpGet("recommendations/{id:int}")]
