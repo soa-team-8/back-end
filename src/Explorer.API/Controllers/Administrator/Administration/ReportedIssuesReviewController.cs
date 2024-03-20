0625f7f4e40439d2fb5b83f911b30f7de18cd404
VukPavlovic425 <vuk.pavlovic425@gmail.com>
2024-03-20 19:34:11 +0100

    feat: reported issues api

--- src/Explorer.API/Controllers/Administrator/Administration/ReportedIssuesReviewController.cs
+++ src/Explorer.API/Controllers/Administrator/Administration/ReportedIssuesReviewController.cs
@@ -6,6 +6,8 @@ using Explorer.Tours.Core.UseCases;
 using Explorer.Tours.Core.UseCases.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Explorer.API.Controllers.Administrator.Administration
 {
@@ -13,46 +15,68 @@ namespace Explorer.API.Controllers.Administrator.Administration
         [Route("api/administration/reportedIssues")]
         public class ReportedIssuesReviewController : BaseApiController
         {
-            private readonly IReportingIssueService _reportedIssueAdministratorService;
+        private readonly HttpClient _httpClient;
 
-            public ReportedIssuesReviewController(IReportingIssueService reportedIssueAdministratorService)
-            {
-                _reportedIssueAdministratorService = reportedIssueAdministratorService;
-            }
+        public ReportedIssuesReviewController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:3000");
+        }
 
-            [HttpGet]
-            public ActionResult<PagedResult<ReportedIssueDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
-            {
-                var result = _reportedIssueAdministratorService.GetPaged(page, pageSize);
-                return CreateResponse(result);
-            }
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<ReportedIssueDto>>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var response = await _httpClient.GetAsync($"/reported-issues");
+            response.EnsureSuccessStatusCode();
+            var contentString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<PagedResult<ReportedIssueDto>>(contentString);
+            return Ok(result);
+        }
 
-            [HttpPost("comment/{id:int}")]
-            public ActionResult<ReportedIssueDto> Respond([FromRoute] int id, [FromBody] ReportedIssueCommentDto ric)
-            {
-                var result = _reportedIssueAdministratorService.AddComment(id, ric);
-                return CreateResponse(result);
-            }
+        [HttpPost("comment/{id:int}")]
+        public async Task<ActionResult<ReportedIssueDto>> Respond([FromRoute] int id, [FromBody] ReportedIssueCommentDto ric)
+        {
+            var json = JsonConvert.SerializeObject(ric);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            [HttpPut("deadline/{id:int}")]
-            public ActionResult<ReportedIssueDto> AddDeadline(int id, [FromBody] DateTime deadline)
-            {
-                var result = _reportedIssueAdministratorService.AddDeadline(id, deadline);
-                return CreateResponse(result);
-            }
+            var response = await _httpClient.PostAsync($"/reported-issues/{id}/comment", content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+            return Ok(result);
+        }
 
-            [HttpPut("penalizeAuthor/{id:int}")]
-            public ActionResult<ReportedIssueDto> PenalizeAuthor(int id)
-            {
-                var result = _reportedIssueAdministratorService.PenalizeAthor(id);
-                return CreateResponse(result);
-            }
+        [HttpPut("deadline/{id:int}")]
+        public async Task<ActionResult<ReportedIssueDto>> AddDeadline(int id, [FromBody] DateTime deadline)
+        {
+            var json = JsonConvert.SerializeObject(deadline);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            [HttpPut("closeReportedIssue/{id:int}")]
-            public ActionResult<ReportedIssueDto> Close(int id)
-            {
-                var result = _reportedIssueAdministratorService.Close(id);
-                return CreateResponse(result);
-            }
+            var response = await _httpClient.PutAsync($"/reported-issues/{id}/deadline", content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+            return Ok(result);
+        }
+
+        [HttpPut("penalizeAuthor/{id:int}")]
+        public async Task<ActionResult<ReportedIssueDto>> PenalizeAuthor(int id)
+        {
+            var response = await _httpClient.PutAsync($"/reported-issues/{id}/penalize", null);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+            return Ok(result);
+        }
+
+        [HttpPut("closeReportedIssue/{id:int}")]
+        public async Task<ActionResult<ReportedIssueDto>> Close(int id)
+        {
+            var response = await _httpClient.PutAsync($"/reported-issues/{id}/close", null);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+            return Ok(result);
+        }
     }
 }
--- src/Explorer.API/Controllers/Tourist/ReportingIssueController.cs
+++ src/Explorer.API/Controllers/Tourist/ReportingIssueController.cs
@@ -6,6 +6,8 @@ using Explorer.Tours.Core.UseCases.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Explorer.API.Controllers.Tourist
 {
@@ -13,53 +15,63 @@ namespace Explorer.API.Controllers.Tourist
     [Route("api/tourist/reportingIssue")]
     public class ReportingIssueController : BaseApiController
     {
-        private readonly IReportingIssueService _reportingIssueService;
+        private readonly HttpClient _httpClient;
 
-        public ReportingIssueController(IReportingIssueService reportingIssueService)
+        public ReportingIssueController(IHttpClientFactory httpClientFactory)
         {
-            _reportingIssueService = reportingIssueService;
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:3000");
         }
 
         [HttpPost("{category}/{description}/{priority}/{tourId}/{touristId}")]
+        public async Task<ActionResult<ReportedIssueDto>> Create(string category, string description, int priority, int tourId, int touristId)
+        {
+            var response = await _httpClient.PostAsync($"/reported-issues/{category}/{description}/{priority}/{tourId}/{touristId}", null);
+            response.EnsureSuccessStatusCode();
 
-        public ActionResult<ReportedIssueDto> Create(string category, string description, int priority, int tourId, int touristId)
-        {
-            ReportedIssueDto reportedIssue = new ReportedIssueDto();
-            reportedIssue.Category = category;
-            reportedIssue.Description = description;
-            reportedIssue.Priority = priority;
-            reportedIssue.TourId = tourId;
-            reportedIssue.TouristId = touristId;
-            reportedIssue.Time= DateTime.UtcNow;
-            reportedIssue.Deadline = null;
-            reportedIssue.Closed = false;
-            reportedIssue.Resolved = false;
-            reportedIssue.Comments = new List<ReportedIssueCommentDto>();
-            reportedIssue.Tour = null;
-            if(reportedIssue.Category.IsNullOrEmpty() || reportedIssue.Priority==0 ||reportedIssue.TourId==0 || reportedIssue.TouristId == 0)
-            {
-                return BadRequest("Fill all the fields.");
-            }
-            var result = _reportingIssueService.Create(reportedIssue);
-            return CreateResponse(result); // na frontu povezati opet sa turom.
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+
+            return Ok(result);
         }
+
         [HttpPut("resolve/{id:int}")]
-        public ActionResult<ReportedIssueDto> Resolve(int id)
+        public async Task<ActionResult<ReportedIssueDto>> Resolve(int id)
         {
-            var result = _reportingIssueService.Resolve(id);
-            return CreateResponse(result);
+            var response = await _httpClient.PutAsync($"/reported-issues/{id}/resolve", null);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+
+            return Ok(result);
         }
+
         [HttpPost("comment/{id:int}")]
-        public ActionResult<ReportedIssueDto> AddComment([FromBody] ReportedIssueCommentDto ric, int id)
+        public async Task<ActionResult<ReportedIssueDto>> AddComment([FromBody] ReportedIssueCommentDto ric, int id)
         {
-            var result = _reportingIssueService.AddComment(id,ric);
-            return CreateResponse(result);
+            var json = JsonConvert.SerializeObject(ric);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"/reported-issues/{id}/comment", content);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReportedIssueDto>(responseContent);
+
+            return Ok(result);
         }
+
         [HttpGet("{id:int}")]
-        public ActionResult<PagedResult<ReportedIssueDto>> GetAllByTourist(int id, [FromQuery] int page, [FromQuery] int pageSize)
+        public async Task<ActionResult<PagedResult<ReportedIssueDto>>> GetAllByTourist(int id, [FromQuery] int page, [FromQuery] int pageSize)
         {
-            var result = _reportingIssueService.GetPagedByTourist(id, page, pageSize);
-            return CreateResponse(result);
+            var response = await _httpClient.GetAsync($"/reported-issues/{id}/tourist");
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            //var result = JsonConvert.DeserializeObject<PagedResult<ReportedIssueDto>>(responseContent);
+
+            return Ok(responseContent);
         }
     }
 }
